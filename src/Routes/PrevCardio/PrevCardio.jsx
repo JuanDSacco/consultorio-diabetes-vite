0362bc3e2c1558f64ae1d90d2368507dc2e0dfbf
juan sacco <juandsacco@gmail.com>
2023-09-21 17:01:26 -0300

    agrego section prev cardio

--- src/Routes/PrevCardio/PrevCardio.jsx
+++ src/Routes/PrevCardio/PrevCardio.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const PrevCardio = () => {
     return (
-        <div>
-        
+        <div className='divPrevCardioContainer'>
+            <h1>Prevención Cardiovascular</h1>
+            <div className='divUlPrevCardio'>
+                <ol> 
+                    <li>Control de presión arterial</li>
+                    <li>Reducir el consumo de sal</li>
+                    <li>Controlar la presencia de sal en las etiquetas de los productos que se consumen</li>
+                    <li>Realizar actividad física todos los dias</li>
+                    <li>Evitar fumar</li>
+                    <li>No descuidar el peso corporal</li>
+                    <li>Respetar las horas de descanso</li>
+                    <li>No olvidar de tomar la medicarión indicada por su médico</li>
+                </ol>
+            </div>
         </div>
     )
 }
